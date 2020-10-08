bca17fba90c8b8bebe20157a71f868a407bc73d5
Karim-Elgenidy <karimsherif2410@aucegypt.edu>
2020-10-09 01:29:14 +0300

    K

--- hangman.h
+++ hangman.h
@@ -14,7 +14,7 @@ QT_BEGIN_NAMESPACE
 namespace Ui { class Hangman; }
 QT_END_NAMESPACE
 
-class Hangman : public QMainWindow, QGraphicsItemAnimation{
+class Hangman : public QMainWindow{
     Q_OBJECT
 
 public:
