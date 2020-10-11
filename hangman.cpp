d8a4b068262e6e718fa6c2e8de9c37bbd0eed945
msolimann22 <msolimann2@aucegypt.edu>
2020-10-11 23:45:53 +0200

    stickman needs fixings

--- hangman.cpp
+++ hangman.cpp
@@ -1,11 +1,35 @@
 #include "hangman.h"
 #include "ui_hangman.h"
 
-Hangman::Hangman(QWidget *parent)
-    : QMainWindow(parent)
-    , ui(new Ui::Hangman)
+Hangman::Hangman(QWidget *parent): QMainWindow(parent), ui(new Ui::Hangman)
 {
     ui->setupUi(this);
+    scene = new QGraphicsScene(scene);
+    ui->graphicsView->setScene(scene);
+    //Drawing of Shapes
+    QBrush BlueBrush(Qt::blue);
+    QBrush BlackBrush(Qt::black);
+    QPen BluePen(Qt::blue);
+
+    BluePen.setWidth(6);
+
+    ellipse = scene -> addEllipse(-70,-190,100,100,BluePen,BlueBrush);
+    line[0]= scene->addLine(-150,-90,100,90, BluePen); //Line 1
+    line[1]= scene->addLine(-150,-90,20,80, BluePen); //Line 2
+    line[2]= scene->addLine(75,-80,20,80, BluePen); //Line 3
+    line[3]= scene->addLine(-10,10,50,100, BluePen); //Line 4
+    line[4]= scene->addLine(10,25,50,100, BluePen); //Line 5
+    ui ->graphicsView->setRenderHint(QPainter::Antialiasing);
+    line[0]->setRotation(53);
+    line[1]->setRotation(60);
+    line[2]->setRotation(300);
+    //line[3]->setRotation(45);
+    line[4]->setRotation(65);
+
+
+    for(int i = 0; i < guessWord.length(); i++){
+        ui->TextBox->setText(ui->TextBox->text() + "*");
+    }
     //initilaizing member variables
     for (int i = 0; i < 30; i++){
         guessedChar[i] = '0';
@@ -36,12 +60,16 @@ void Hangman::keyPressEvent(QKeyEvent *event)
         //we add the character to the guessedChar array
         back++;
         guessedChar[back] = event->key();
+        ui->TextBox_2->setText(ui->TextBox_2->text() + " " + QString(QChar(guessedChar[back])));
 
         //looping through guessword
         for(int i=0; i<guessWord.length() ; i++){
 
             if(event->key() == guessWord.at(i) ){
                 flag = 1;
+                QString text = ui->TextBox->text();
+                text[i] = event->key();
+                ui->TextBox->setText(text);
             }
 
         }
@@ -50,10 +78,11 @@ void Hangman::keyPressEvent(QKeyEvent *event)
 
         if(flag==0){
             //ADD CODE TO DRAW SHAPES HERE !!!!!!!!
-            ui->TextBox->setText("guessed incorrectly");
+            ui->label->setText("guessed incorrectly");
         }else{
             //ADD CODE TO DRAW CHARACTERS AND WORD HERE !!!!!!!!!!!!!
-            ui->TextBox->setText("guessed correctly");
+           //ui->TextBox->setText(ui->TextBox->text() + " " + QString(QChar(guessedChar[back])) );
+            ui->label->setText("guessed correctly");
         }
     }
 
